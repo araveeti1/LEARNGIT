b75c6571941452359c90e22946f5234f6f54b4c4
Demo Scotiobank <venkatestscotioa@gmail.com>
2023-04-15 20:18:31 -0400

    committed the changes in caps manager in Branch1 into feature branch

--- appium-cucumberbdd-testng-master/src/main/java/com/qa/utils/CapabilitiesManager.java
+++ appium-cucumberbdd-testng-master/src/main/java/com/qa/utils/CapabilitiesManager.java
@@ -15,7 +15,7 @@ public class CapabilitiesManager {
         Properties props = new PropertyManager().getProps();
 
         try{
-            utils.log().info("getting desired capabilities.This is main Branch.committed the latest code.");
+            utils.log().info("getting desired capabilities!!!");
             DesiredCapabilities caps = new DesiredCapabilities();
             caps.setCapability(MobileCapabilityType.PLATFORM_NAME, params.getPlatformName());
             caps.setCapability(MobileCapabilityType.UDID, params.getUDID());
